46b78dd07c6926a491e090ecab9637147cd335fa
Little-Red-Cap <little-red-cap@foxmail.com>
2024-07-18 02:56:20 +0800

    update

--- apps/draft/undefined/main.cpp
+++ apps/draft/undefined/main.cpp
@@ -4,3 +4,9 @@ int main() {
     return 0;
 }
 
+// arm studio
+//https://developer.arm.com/Tools%20and%20Software/Arm%20Development%20Studio
+// arm-clang
+//https://developer.arm.com/Tools%20and%20Software/Arm%20Compiler%20for%20Embedded
+// arm-none-eabi-gcc
+//https://developer.arm.com/Tools%20and%20Software/GNU%20Toolchain
